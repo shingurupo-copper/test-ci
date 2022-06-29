7a4bf44eb30d356d736f9dacc2c95ed388526de9
COPPER Nguyen <dong.nguyen.04@shiftasia.com>
2022-06-29 16:11:31 +0700

    install for arm

--- install_aws_cli.sh
+++ install_aws_cli.sh
@@ -12,17 +12,18 @@ apt-get install -y \
     dnsutils
 
 
-# install docker cli
+# install for amd64 and arm
 if [[ $(uname -m) == "arm64" || $(uname -m) == "aarch64" ]]; then
     wget --no-check-certificate "https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/arm64/docker-ce-cli_20.10.17~3-0~ubuntu-focal_arm64.deb"
     dpkg -i "docker-ce-cli_20.10.17~3-0~ubuntu-focal_arm64.deb"
+    curl -O "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip" && unzip awscliv2.zip
+
 else
     wget --no-check-certificate "https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce-cli_20.10.17~3-0~ubuntu-focal_amd64.deb"
     dpkg -i "docker-ce-cli_20.10.17~3-0~ubuntu-focal_amd64.deb"
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip
 fi
 docker info
-# install aws cli
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip
-# ./aws/install
-# aws --version
-# aws sts get-caller-identity
+./aws/install
+aws --version
+aws sts get-caller-identity
