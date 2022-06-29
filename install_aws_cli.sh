2c6c9ebd6c55ff15aeeae2cf50b13faee00b00b3
COPPER Nguyen <dong.nguyen.04@shiftasia.com>
2022-06-29 15:53:26 +0700

    haha

--- install_aws_cli.sh
+++ install_aws_cli.sh
@@ -20,9 +20,6 @@ else
     wget --no-check-certificate "https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce-cli_20.10.17~3-0~ubuntu-focal_amd64.deb"
     dpkg -i "docker-ce-cli_20.10.17~3-0~ubuntu-focal_amd64.deb"
 fi
-nslookup docker
-nslookup DOCKER_HOST
-export DOCKER_HOST=tcp://172.17.0.1:2375
 docker info
 # install aws cli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip
