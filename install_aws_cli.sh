8b2ac556fbc87b7af71c9a8107392144a5d019a4
COPPER Nguyen <dong.nguyen.04@shiftasia.com>
2022-06-29 15:46:39 +0700

    add ubuntu

---
+++ install_aws_cli.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    zip \
+    unzip \
+    wget
+
+# install docker cli
+if [[ $(uname -m) == "arm64" || $(uname -m) == "aarch64" ]]; then
+    wget --no-check-certificate "https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/arm64/docker-ce-cli_20.10.17~3-0~ubuntu-focal_arm64.deb"
+    dpkg -i "docker-ce-cli_20.10.17~3-0~ubuntu-focal_arm64.deb"
+else
+    wget --no-check-certificate "https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce-cli_20.10.17~3-0~ubuntu-focal_amd64.deb"
+    dpkg -i "docker-ce-cli_20.10.17~3-0~ubuntu-focal_amd64.deb"
+fi
+nslookup DOCKER_HOST
+nslookup docker
+export DOCKER_HOST=tcp://172.17.0.1:2375
+docker info
+# install aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip
+./aws/install
+aws --version
+aws sts get-caller-identity
